6ae5e987b182e77d817fe38482790bd502f82f8e
simonostini <simonostini@gmail.com>
2025-10-03 11:56:32 +0200

    vault backup: 2025-10-03 11:56:32

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from models.shell import Shell
+from models.deck import Deck
 
 
 def main():
-    shell: Shell = Shell()
-    shell.cmdloop()
+    # shell: Shell = Shell()
+    # shell.cmdloop()
+
+    deck: Deck = Deck()
+    
+    print() for suit in new_deck_order_suits for rank in new_deck_order_ranks]
 
 
 if __name__ == "__main__":
