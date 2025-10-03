ceff1f12843877c5a9974051fcd28a531d6838a7
simonostini <simonostini@gmail.com>
2025-10-03 11:41:32 +0200

    vault backup: 2025-10-03 11:41:32

--- src/models/deck.py
+++ src/models/deck.py
@@ -8,13 +8,10 @@ from enums.rank import Rank
 class Deck:
     cards: list[Card] = []
 
-    def __init__(self, cards=[]):
+    def __init__(self, cards):
         self.cards = cards
 
     def get_new_deck() -> Deck:
-        cards = [Card(suit, rank) for (suit, rank) in product()]
-        for suit in Suit:
-            for rank in Rank:
-                cards.append(Card(suit, rank))
+        cards = [Card(suit, rank) for suit in Suit for rank in Rank]
 
         return Deck(cards)
