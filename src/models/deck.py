00783855d9a872bf8be1986a0dacfdc198378722
simonostini <simonostini@gmail.com>
2025-10-03 11:47:32 +0200

    vault backup: 2025-10-03 11:47:32

--- src/models/deck.py
+++ src/models/deck.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import random
 
 from models.card import Card
 from enums.suit import Suit
@@ -9,11 +10,14 @@ class Deck:
     cards: list[Card] = []
 
     def __init__(self, cards: list[Card] = []) -> None:
-            self.cards = cards if cards else self.get_new_sorted_cards()
+        self.cards = cards if cards else self.get_new_sorted_cards()
 
     def get_new_sorted_cards() -> list[Card]:
         return [Card(suit, rank) for suit in Suit for rank in Rank]
-    
-    def shuffle() -> None:
-         
 
+    def shuffle(numbers: list[int]) -> list[int]:
+        for i in range(len(numbers) - 1, 0, -1):
+            j = random.randint(0, i)
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+
+        return numbers
