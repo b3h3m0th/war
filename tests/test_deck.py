e2a5ed0e3bc8ccec93d63953f5b2e5ec8b327eb8
simonostini <simonostini@gmail.com>
2025-10-03 21:12:25 +0200

    vault backup: 2025-10-03 21:12:25

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -4,3 +4,8 @@ from models.deck import Deck
 def test_initial_deck_cards_count() -> None:
     deck = Deck()
     assert len(deck.cards) == 52
+
+
+def test_initial_deck_cards_order() -> None:
+    deck = Deck()
+    assert  == 52
