476412c6636b64dcc16e97ca1e1e611c16723207
simonostini <simonostini@gmail.com>
2025-10-03 10:45:28 +0200

    vault backup: 2025-10-03 10:45:28

--- src/models/shell.py
+++ src/models/shell.py
@@ -25,43 +25,6 @@ class Shell(Cmd):
     prompt = "(turtle) "
     file = None
 
-    # ----- basic turtle commands -----
-    def do_forward(self, arg):
-        "Move the turtle forward by the specified distance:  FORWARD 10"
-        forward(*parse(arg))
-
-    def do_right(self, arg):
-        "Turn turtle right by given number of degrees:  RIGHT 20"
-        right(*parse(arg))
-
-    def do_left(self, arg):
-        "Turn turtle left by given number of degrees:  LEFT 90"
-        left(*parse(arg))
-
-    def do_goto(self, arg):
-        "Move turtle to an absolute position with changing orientation.  GOTO 100 200"
-        goto(*parse(arg))
-
-    def do_home(self, arg):
-        "Return turtle to the home position:  HOME"
-        home()
-
-    def do_circle(self, arg):
-        "Draw circle with given radius an options extent and steps:  CIRCLE 50"
-        circle(*parse(arg))
-
-    def do_position(self, arg):
-        "Print the current turtle position:  POSITION"
-        print("Current position is %d %d\n" % position())
-
-    def do_heading(self, arg):
-        "Print the current turtle heading in degrees:  HEADING"
-        print("Current heading is %d\n" % (heading(),))
-
-    def do_color(self, arg):
-        "Set the color:  COLOR BLUE"
-        color(arg.lower())
-
     def do_undo(self, arg):
         "Undo (repeatedly) the last turtle action(s):  UNDO"
 
@@ -69,7 +32,7 @@ class Shell(Cmd):
         "Clear the screen and return turtle to center:  RESET"
         reset()
 
-    def do_q(self, arg):
+    def do_stop(self, arg):
         "Stop recording, close the turtle window, and exit:  BYE"
         print("Thank you for using")
         self.close()
