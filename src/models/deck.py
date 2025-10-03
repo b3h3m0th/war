d74604fb091d6357d9ce9be2b02edfbe09412697
simonostini <simonostini@gmail.com>
2025-10-03 11:49:25 +0200

    implemented shuffle

--- src/models/deck.py
+++ src/models/deck.py
@@ -15,9 +15,12 @@ class Deck:
     def get_new_sorted_cards() -> list[Card]:
         return [Card(suit, rank) for suit in Suit for rank in Rank]
 
-    def shuffle(numbers: list[int]) -> list[int]:
-        for i in range(len(numbers) - 1, 0, -1):
-            j = random.randint(0, i)
-            numbers[i], numbers[j] = numbers[j], numbers[i]
+    def shuffle(self, cards: list[Card]) -> list[int]:
+        if not cards:
+            cards = self.cards
 
-        return numbers
+        for i in range(len(cards) - 1, 0, -1):
+            j = random.randint(0, i)
+            cards[i], cards[j] = cards[j], cards[i]
+
+        return cards
