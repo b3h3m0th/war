e17ac56a9b3121e4befe18f13503ff0f00a35f6e
simonostini <simonostini@gmail.com>
2025-10-03 11:38:33 +0200

    vault backup: 2025-10-03 11:38:33

--- src/models/deck.py
+++ src/models/deck.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from models.card import Card
 from enums.suit import Suit
 from enums.rank import Rank
@@ -9,7 +11,7 @@ class Deck:
     def __init__(self, cards=[]):
         self.cards = cards
 
-    def get_new_deck():
+    def get_new_deck() -> Deck:
         cards = []
         for suit in Suit:
             for rank in Rank:
