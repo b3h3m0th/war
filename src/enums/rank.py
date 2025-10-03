6a80814c0b62b46becfec9d137260f854993b83a
simonostini <simonostini@gmail.com>
2025-10-03 10:05:16 +0200

    added rank enum

--- src/enums/rank.py
+++ src/enums/rank.py
@@ -2,7 +2,12 @@ from enum import Enum
 
 
 class Rank(Enum):
-    Two: int = "Spades"
-    Three: int = 
-    Four: int = 
-    Five: int = 
+    Two: int = 2
+    Three: int = 3
+    Four: int = 4
+    Five: int = 5
+    Six: int = 6
+    Seven: int = 7
+    Eight: int = 8
+    Nine: int = 9
+    Ten: int = 10
