0f602b29d852eddbeee9fd4da5702e624a88ba6c
simonostini <simonostini@gmail.com>
2025-10-03 11:32:51 +0200

    bootstrapped deck class

--- src/models/deck.py
+++ src/models/deck.py
@@ -1,6 +1,13 @@
 from models.card import Card
+from enums.suit import Suit
+from enums.rank import Rank
 
 
 class Deck:
     cards: list[Card] = []
 
+    def __init__(self):
+        cards = []
+
+    def get_new_deck():
+        pass
