ed85b9180183eee683928896ab51120017a800c4
simonostini <simonostini@gmail.com>
2025-10-03 10:19:30 +0200

    vault backup: 2025-10-03 10:19:30

--- src/models/menu.py
+++ src/models/menu.py
@@ -1,3 +1,7 @@
+class Menu():
+
+
+
 def print_menu():
     print()
     print("┌──────────────────────────────────┐")
@@ -16,7 +20,24 @@ def print_menu():
 
 
 def main():
-    menu: Menu = Menu()
+    register = {}
+
+    while True:
+        print_menu()
+        option = input(">> ")
+        if option == "R":
+            name = input("\nEnter name: ")
+            points = int(input("Enter points: "))
+            if name in register:
+                register[name] += points
+            else:
+                register[name] = points
+        elif option == "S":
+            display_grades(register)
+        elif option == "Reset":
+            register = {}
+        elif option == "E":
+            break
 
 
 if __name__ == "__main__":
