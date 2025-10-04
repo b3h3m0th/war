5183963de250d19bc29643aad065e51045d62840
simonostini <simonostini@gmail.com>
2025-10-04 04:09:24 +0200

    vault backup: 2025-10-04 04:09:24

--- src/models/player.py
+++ src/models/player.py
@@ -1,16 +1,17 @@
+import json
+from annotations import __future__
+
 class Player:
     name: str
 
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return {
             "name": self.name,
         }
 
     @classmethod
-    def from_dict(cls, data):
-        # Deserialize nested object
-        address = Address.from_dict(data["address"])
+    def from_dict(cls, data: ) -> Player:
         return cls(data["name"], data["age"], address)
