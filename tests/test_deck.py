a8ca2bb4f5630079ff4881ec26f2786f8bd0d99e
simonostini <simonostini@gmail.com>
2025-10-04 03:55:44 +0200

    fix lint

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -6,7 +6,7 @@ from models.card import Card
 
 def test_initial_deck_cards_count() -> None:
     deck = Deck()
-    assert len(deck.cards) is 52
+    assert len(deck.cards) == 52
 
 
 def test_initial_deck_cards_order() -> None:
