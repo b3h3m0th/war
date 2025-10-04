a16ac4c630a3bb34107cf845641c839d9fa9dfff
simonostini <simonostini@gmail.com>
2025-10-04 04:06:24 +0200

    vault backup: 2025-10-04 04:06:24

--- src/models/serializer.py
+++ src/models/serializer.py
@@ -1,11 +1,14 @@
 import json
+from pathlib import Path
 
 
 class Serializer:
-    def save(obj, path) -> None:
+    @staticmethod
+    def save(obj: any, path: Path | str) -> None:
         with open(path, "w") as f:
             json.dump(obj.to_dict(), f, indent=4)
 
+    @staticmethod
     def load(cls, path) -> None:
         with open(path, "r") as f:
             return cls.from_dict(json.load(f))
