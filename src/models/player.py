24f2720e4e65937c25526b509986f40da784ecb1
simonostini <simonostini@gmail.com>
2025-10-04 04:08:24 +0200

    vault backup: 2025-10-04 04:08:24

--- src/models/player.py
+++ src/models/player.py
@@ -3,3 +3,14 @@ class Player:
 
     def __init__(self, name: str) -> None:
         self.name = name
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        # Deserialize nested object
+        address = Address.from_dict(data["address"])
+        return cls(data["name"], data["age"], address)
--- src/models/serializer.py
+++ src/models/serializer.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 class Serializer:
     @staticmethod
-    def save(obj: any, path: Path | str) -> None:
+    def save(obj: object, path: Path | str) -> None:
         with open(path, "w") as f:
             json.dump(obj.to_dict(), f, indent=4)
 
     @staticmethod
-    def load(cls, path) -> None:
+    def load(cls, path: Path | str) -> None:
         with open(path, "r") as f:
             return cls.from_dict(json.load(f))
