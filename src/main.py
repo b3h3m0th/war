ea62da0db11e2760544c0c02de37e64a8b1aca65
simonostini <simonostini@gmail.com>
2025-10-08 13:36:29 +0200

    vault backup: 2025-10-08 13:36:29

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from models.shell import Shell
+from models.card import Card
+from enums.rank import Rank
+from enums.suit import Suit
 
 
 def main() -> None:
     shell: Shell = Shell()
     shell.cmdloop()
+
+    card1 = Card(Suit.Clubs, Rank.Ace)
 
 
 if __name__ == "__main__":
