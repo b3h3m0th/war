39c9d644164202b39b37ebcb16f50f20caabea0c
simonostini <simonostini@gmail.com>
2025-10-08 13:38:29 +0200

    vault backup: 2025-10-08 13:38:29

--- src/main.py
+++ src/main.py
@@ -5,10 +5,14 @@ from enums.suit import Suit
 
 
 def main() -> None:
-    shell: Shell = Shell()
-    shell.cmdloop()
+    # shell: Shell = Shell()
+    # shell.cmdloop()
 
     card1 = Card(Suit.Clubs, Rank.Ace)
+    card2 = Card(Suit.Clubs, Rank.King)
+    shell = Shell()
+
+    print(card1.__eq__(shell))
 
 
 if __name__ == "__main__":
