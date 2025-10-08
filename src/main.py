4d00bc739c4ed3851ac69635ad8b8452e4f0776f
simonostini <simonostini@gmail.com>
2025-10-08 13:40:29 +0200

    vault backup: 2025-10-08 13:40:29

--- src/main.py
+++ src/main.py
@@ -10,9 +10,6 @@ def main() -> None:
 
     card1 = Card(Suit.Clubs, Rank.Ace)
     card2 = Card(Suit.Clubs, Rank.King)
-    shell = Shell()
-
-    print(card1.__eq__(shell))
 
 
 if __name__ == "__main__":
