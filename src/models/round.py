01afb3b6c6655f9d09474d39c3a0a8fc97ca191f
Ted J Burgess <ted.j.burgess@gmail.com>
2025-10-10 15:17:49 +0200

    Created boilerplate for round class

---
+++ src/models/round.py
@@ -0,0 +1,15 @@
+from models.turn import Turn
+from enums.suit import Suit
+
+class Round:
+    turns: list[Turn]
+
+    def __init__(self, turns):
+        self.turns = turns
+
+    def get_winning_round(self) -> Turn:
+        if self.turns[0].card > self.turns[1]:
+            return self.turns(0)
+        elif self.turns[1].card > self.turns[0]:
+            return self.turns(1)
+    
