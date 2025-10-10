1d1bdc297ed2d264c46c0eb1d3176b8e301b9843
Osama Alsaheb <sahebosama12@gmail.com>
2025-10-10 15:12:30 +0200

    Added isNPC property to player class

--- src/models/player.py
+++ src/models/player.py
@@ -2,12 +2,16 @@ from __future__ import annotations
 
 
 class Player:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, isNPC: bool) -> None:
         self.name = name
+        self.isNPC = bool
+        if (self.isNPC):
+            self.name = "Computer"
 
     def to_dict(self) -> dict:
         return {
             "name": self.name,
+            "isNPC": self.isNPC
         }
 
     @classmethod
