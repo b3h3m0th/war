35ccdfa42274f03a6216731b522d9fce29651c5f
Osama Alsaheb <sahebosama12@gmail.com>
2025-10-10 14:07:26 +0200

    Added a validation loop to enforce correct player selection and removed the card property from game class.

--- src/models/game.py
+++ src/models/game.py
@@ -1,19 +1,16 @@
 from models.deck import Deck
 from models.player import Player
 from enums.variant import Variant
-from models.card import Card
 
 
 class Game:
     players: list[Player]
     deck: Deck
     variant: Variant
-    card: Card
 
     def __init__(self, players: list[Player], variant: Variant) -> None:
         self.players = players
         self.variant = variant
-        self.card = Card(suit=None, rank=None)
         self.deck = Deck()
         self.deck.shuffle(cards=[])
 
@@ -44,9 +41,17 @@ class Game:
             "Computer (pvc) or Player vs Player (pvp)?"
         )
         player_choice = input().strip().lower()
-        if player_choice == "pvc":
-            self.do_pvc(self)
-        else:
-            self.do_pvp(self)
+        while (player_choice!= "pvc" or player_choice !="pvp"):
+            if player_choice == "pvc":
+                self.do_pvc(self)
+                break
+            elif player_choice == "pvp":
+                self.do_pvp(self)
+                break
+            else:
+                print(f"Incorrect input try again.Would you like to play Player vs "
+            "Computer (pvc) or Player vs Player (pvp)?")
+                player_choice = input().strip().lower()
+
 
         self.deck.shuffle(None)
