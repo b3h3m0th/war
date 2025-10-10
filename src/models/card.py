e4a10fa22a9d3c2685709985331765fc26f5c5e1
simonostini <simonostini@gmail.com>
2025-10-10 15:22:29 +0200

    removed module code

--- src/models/card.py
+++ src/models/card.py
@@ -19,16 +19,28 @@ RANK_STRINGS = {
 
 class Card:
     def __init__(self, suit: Suit, rank: Rank) -> None:
+        """
+        Instantiates a new Card with a given suite and rank
+        """
+
         self.suit = suit
         self.rank = rank
 
     def __str__(self) -> str:
+        """
+        Displays a card as a string with the according symbol of its suit
+        """
+
         suit_string = SUIT_STRINGS.get(self.suit)
         rank_string = RANK_STRINGS.get(self.rank, str(self.rank.value))
 
         return f"{rank_string}{suit_string}"
 
     def __eq__(self, other) -> bool:
+        """
+        Checks whether a card is equal to another card
+        """
+
         return (
             isinstance(other, Card)
             and self.suit == other.suit
@@ -36,15 +48,23 @@ class Card:
         )
 
     def __hash__(self) -> int:
+        """
+        Computes a hash of a card based on its suit and rank
+        """
+
         return hash((self.suit, self.rank))
 
     def to_dict(self) -> dict:
+        """
+        Converts a card into a dictionary that can be stringified into json
+        """
+
         return {"suit": self.suit, "rank": self.rank}
 
     @classmethod
     def from_dict(cls, data: dict) -> Card:
+        """
+        Creates and returns a card based on a json dictionary
+        """
+
         return cls(data["suit"], data["rank"])
-
-
-if __name__ == "__main__":
-    print("hi from card module")
