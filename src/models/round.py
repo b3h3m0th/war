f2550355d8eceebb1972798e5b8f1db74c95872a
simonostini <simonostini@gmail.com>
2025-10-12 10:42:19 +0200

    Add Game Round and Turn class structure and logic

--- src/models/round.py
+++ src/models/round.py
@@ -4,11 +4,7 @@ from models.turn import Turn
 class Round:
     turns: list[Turn]
 
-    def __init__(self, turns):
+    def __init__(self, turns = []):
         self.turns = turns
 
     def get_winning_round(self) -> Turn:
-        if self.turns[0].card > self.turns[1]:
-            return self.turns(0)
-        elif self.turns[1].card > self.turns[0]:
-            return self.turns(1)
