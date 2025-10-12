6ef6dfd091a3194d558382a5262f96763cf799d8
simonostini <simonostini@gmail.com>
2025-10-12 10:47:56 +0200

    implemented get winning turn method

--- src/models/round.py
+++ src/models/round.py
@@ -4,7 +4,21 @@ from models.turn import Turn
 class Round:
     turns: list[Turn]
 
-    def __init__(self, turns = []):
+    def __init__(self, turns=[]):
         self.turns = turns
 
-    def get_winning_round(self) -> Turn:
+    def get_winning_turn(self) -> Turn | None:
+        """
+        Returns the turn of the round with the highest played card by rank.
+        Returns None if the round has no turns or there is a tie.
+        """
+
+        winning_turn: Turn | None = None
+
+        for turn in self.turns:
+            if not winning_turn or turn.card.rank > winning_turn.card.rank:
+                winning_turn = turn
+            elif turn.card.rank == winning_turn.rank:
+                winning_turn = None
+
+        return winning_turn
