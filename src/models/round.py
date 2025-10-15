063cf13876c0fdb53fc208236aaf63bbff8d18cb
simonostini <simonostini@gmail.com>
2025-10-15 15:33:36 +0200

    removed backup of winning turns method

--- src/models/round.py
+++ src/models/round.py
@@ -11,21 +11,6 @@ class Round:
 
         self.turns = turns
 
-    def get_winning_turns2(self) -> list[Turn]:
-        """
-        Returns a list of turns with the highest cards by rank.
-        Returns a a list of mulitple turns in case of a tie.
-        Returns an empty list if there are no turns.
-        """
-
-        if not self.turns:
-            return []
-
-        max_rank = max(turn.card.rank.value for turn in self.turns)
-        return [
-            turn for turn in self.turns if turn.card.rank.value == max_rank
-        ]
-
     def get_winning_turns(self) -> list[Turn]:
         """
         Returns a list of turns with the highest cards by rank.
