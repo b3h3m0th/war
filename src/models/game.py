3afc82ee073c420954a7676f326f025b03a34bdf
simonostini <simonostini@gmail.com>
2025-10-15 21:01:18 +0200

    added mode selection

--- src/models/game.py
+++ src/models/game.py
@@ -2,6 +2,8 @@ from models.deck import Deck
 from models.player import Player
 from enums.variant import Variant
 
+from prompt_toolkit.shortcuts import choice
+
 
 class Game:
     def __init__(
@@ -10,18 +12,23 @@ class Game:
         self.players = players
         self.variant = variant
         self.deck = Deck()
-        self.deck.shuffle(cards=[])
 
-    def do_pvc(self, arg) -> None:
-        "Player vs Computer"
+    def pvc(self, arg) -> None:
+        """
+        Runs the game in Player vs Computer mode
+        """
+
         print("Player vs Computer mode")
         player = Player(name=input("Enter your name: ").strip())
         computer = Player(name="Computer")
         self.players = [player, computer]
         print(f"Players: {self.players[0].name} vs {self.players[1].name}")
 
-    def do_pvp(self, arg) -> None:
-        "Player vs Player"
+    def pvp(self, arg) -> None:
+        """
+        Runs the game in Player vs Player mode
+        """
+
         print("Player vs Player mode")
         player1 = Player(name=input("Enter name for Player 1: ").strip())
         player2 = Player(name=input("Enter name for Player 2: ").strip())
@@ -29,24 +36,22 @@ class Game:
         print(f"Players: {self.players[0].name} vs {self.players[1].name}")
 
     def start(self) -> None:
-        print("Starting the game...")
-        print(
-            "Would you like to play Player vs "
-            "Computer (pvc) or Player vs Player (pvp)?"
-        )
-        player_choice = input().strip().lower()
-        while player_choice != "pvc" or player_choice != "pvp":
-            if player_choice == "pvc":
-                self.do_pvc(self)
-                break
-            elif player_choice == "pvp":
-                self.do_pvp(self)
-                break
-            else:
-                print(
-                    "Incorrect input try again.Would you like to play Playe"
-                    "r vs Computer (pvc) or Player vs Player (pvp)?"
-                )
-                player_choice = input().strip().lower()
+        """
+        Starts the game
+        """
 
-        self.deck.shuffle(None)
+        player_choice = choice(
+            message="Choose a gamemode",
+            options=[
+                ("pvc", "Player vs Computer"),
+                ("pvp", "Player vs Player"),
+            ],
+            default="pvc",
+        )
+
+        if player_choice == "pvc":
+            self.pvc(self)
+        elif player_choice == "pvp":
+            self.pvp(self)
+
+        self.deck.shuffle()
